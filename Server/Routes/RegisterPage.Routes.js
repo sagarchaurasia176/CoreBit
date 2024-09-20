67700724ebd75e84d9e0238726a474396e6cd16e
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-09-20 17:42:56 +0530

    some minor changed in cookies

--- Server/Routes/RegisterPage.Routes.js
+++ Server/Routes/RegisterPage.Routes.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const {RegisterPage} = require('../controller/Register.Controller')
+const express = require("express");
+const router = express.Router();
+const { RegisterPage } = require("../controller/Register.Controller");
+const { LoginController } = require("../controller/Login.Controller");
+const { Logout } = require("../controller/Logout.Controller");
+
+
 //Register Page applied there so we get !
-router.post('/api/RegisterPage',RegisterPage);
+router.post("/api/RegisterPage", RegisterPage);
+router.post("/api/LoginPage", LoginController);
+router.get('/api/LoggedOut' , Logout);
+
+// blog page routes
 
 
-module.exports = router
+module.exports = router;
---
+++ Server/controller/Logout.Controller.js
@@ -0,0 +1,23 @@
+// Log-Out method applied there so we get
+const cookie = require("cookie");
+exports.Logout = async (req, res) => {
+  try {
+    const cookiesLoggedOut = res.clearCookie("userToken", {
+      httpOnly: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "You Logged out !",
+      data: cookiesLoggedOut,
+    });
+
+    //Error parts
+  } catch (er) {
+    res.status(402).json({
+      success: false,
+      message: "You're not Logged out !",
+      error: er.message,
+    });
+  }
+};
