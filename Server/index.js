ecdbc36e123d905dd436f0f87d6af624a598efa5
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-10-30 22:46:21 +0530

    minor chnage in env

--- Server/index.js
+++ Server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // CORS configuration to allow frontend connection
 app.use(
   cors({
-    origin: "http://localhost:5173" ,
+    origin: process.env.FRONTEND,
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
