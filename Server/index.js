b5c153db7f4fa43dc5aaf8e42b4fda7885021bfd
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-23 23:29:52 +0530

    removed dir_name logic

--- Server/index.js
+++ Server/index.js
@@ -52,11 +52,6 @@ dbConnection();
 // Define routes
 app.use("/api/v1", blogRouter);
 
-// Basic route for the server
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"))
-});
-
 
 const port = process.env.PORT || 8000
 app.listen(port , ()=>{
