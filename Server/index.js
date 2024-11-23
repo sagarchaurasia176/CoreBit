dda0aebd1a355fdc773223dfffc50f726a91c1d4
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-24 00:07:09 +0530

    cors error resolved

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // CORS configuration to allow frontend connection
 app.use(
   cors({
-    origin: "*",
+    origin: "*" || process.env.FRONTEND,
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
