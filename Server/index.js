d3aa0bbbc860f68838d5976584781de58f858749
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-24 00:03:08 +0530

    cors error

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,14 @@
 // Import necessary modules
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const expressFileUpload = require("express-fileupload");
-require('dotenv').config()
+require("dotenv").config();
 // Load environment variables
 
 // Initialize Express app
 const app = express();
-const port = process.env.PORT || 8000
-
+const port = process.env.PORT || 8000;
 
 // Database and Cloudinary configurations
 const dbConnection = require("./config/MongoDb");
@@ -23,15 +22,12 @@ app.use(cookieParser());
 // CORS configuration to allow frontend connection
 app.use(
   cors({
-    origin: process.env.FRONTEND,
+    origin: "*",
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-   
   })
 );
-
-
 
 // File upload configuration using express-fileupload
 app.use(
@@ -40,7 +36,6 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-
 
 // Initialize Cloudinary
 cloudinary();
@@ -51,10 +46,10 @@ dbConnection();
 // Define routes
 app.use("/api/v1", blogRouter);
 
-app.get('/' , (req,res)=>{
+app.get("/", (req, res) => {
   res.send("server working fine");
-})
-app.listen(port , ()=>{
-    console.log("port" , port)
-})
+});
+app.listen(port, () => {
+  console.log("port", port);
+});
 module.exports = app;
