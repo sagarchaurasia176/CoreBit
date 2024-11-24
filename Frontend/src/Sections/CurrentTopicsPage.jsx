c930812ed8f098a0d5a5cd014556d0c46db7af9f
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-24 17:46:39 +0530

    minor changes in package

--- Frontend/src/Sections/CurrentTopicsPage.jsx
+++ Frontend/src/Sections/CurrentTopicsPage.jsx
@@ -14,6 +14,7 @@ const CurrentTopicsPage = () => {
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
+    // des
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
